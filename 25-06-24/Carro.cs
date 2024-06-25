fb0194f119c17b1b741cc7dbcde669f89fdc78c3
Luis Henrique Cunha Cardoso <LuisHeenrique>
2024-06-25 20:43:50 -0300

    Upando desafio

---
+++ 25-06-24/Carro.cs
@@ -0,0 +1,19 @@
+public class Carro : Veiculo
+{
+    public void Buzinar()
+    {
+        Console.WriteLine("Carro buzinando");
+    }
+
+    public override void Acelerar()
+    {
+        velocidade = velocidade + 10;
+        Console.WriteLine("Carro acelerando");
+    }
+
+    public override void Frear()
+    {
+        velocidade = velocidade - 5;
+        Console.WriteLine("Carro freando");
+    }
+}
---
+++ 25-06-24/Program.cs
@@ -0,0 +1,36 @@
+﻿Carro c1 = new Carro();
+
+Moto m1 = new Moto();
+
+//criar array de inteiros de tamanho 5
+int[] notas = new int[5];
+// atribui valores ao array
+notas[0] = 90;
+notas[1] = 85;
+notas[2] = 88;
+notas[3] = 92;
+notas[4] = 95;
+
+//acessando e imprimindo as notas dos alunos
+for (int i = 0; i < notas.Length; i++) 
+{
+    Console.WriteLine($"Nota do Aluno {i + 1}: {notas[i]}");
+}
+//Cria uma lista vazia de veiculos
+List<Veiculo> veiculos = new List<Veiculo>();
+
+//adicionar veiculos
+veiculos.Add(c1);
+veiculos.Add(m1);
+
+//remover veiculos
+
+
+
+foreach(Veiculo veiculo in veiculos)
+{
+    veiculo.Acelerar();
+    veiculo.ExibirVelocidade();
+    veiculo.Frear();
+    veiculo.ExibirVelocidade();
+}
